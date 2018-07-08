35e20cc08dc9c7dd97b71323d5acbaeb979945cb
kajal <kajolsingh88088@gmail.com>
2018-07-08 17:33:12 +0530

    array in js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,8 +5,8 @@ function myFunction() {
 }
 
 const colors = document.getElementById("rainbow");
-const  array = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
+const  diffrent = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
 
-for(var i = 0; i < array.length; i ++;){
-  colors[i].style.color = array[i];
+for(var i = 0; i < diffrent.length; i ++;){
+  colors[i].style.color = diffrent[i];
 }
