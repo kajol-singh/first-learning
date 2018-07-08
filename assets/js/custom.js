2cb400d01f48ef76f2ecf63186e9d26e8600bfd2
kajal <kajolsingh88088@gmail.com>
2018-07-08 17:52:00 +0530

    array in js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,7 +4,7 @@ function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 
-
+ var i = 0;
 function mylisting(){
 const listItems = document.getElementById('rainbow');
 const colors = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
