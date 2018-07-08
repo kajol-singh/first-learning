438a4dc8118ea2c46df0b971c2deffdc996fa333
kajal <kajolsingh88088@gmail.com>
2018-07-08 17:15:49 +0530

    array in js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,3 +3,10 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
+
+var colors = document.getElementById("rainbow");
+var array = [#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
+
+for(var i = 0; i < array.length; i++;){
+  colors[i].style.color = array[i];
+}
