657c9530adaf68bbcf991f770540fae4a395f475
kajal <kajolsingh88088@gmail.com>
2018-07-08 17:57:13 +0530

    array in js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,7 +5,7 @@ function myFunction() {
 }
 
  var i = 0;
-function mylisting(){
+function myFunction(){
 const listItems = document.getElementById('rainbow');
 const colors = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
 
