2f36d58e58a288f3ab5d14c4a3d613cf400206f9
kajal <kajolsingh88088@gmail.com>
2018-07-08 17:19:01 +0530

    array in js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,6 +7,6 @@ function myFunction() {
 var colors = document.getElementById("rainbow");
 var array = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
 
-for(var i = 0; i < array.length; i++;){
+for(var i = 0; i < array.length; i ++;){
   colors[i].style.color = array[i];
 }
