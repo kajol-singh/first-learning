71663e9e105a33d7d195fb9daf19cbffdb3f8beb
kajal <kajolsingh88088@gmail.com>
2018-07-08 17:19:59 +0530

    array in js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,8 +4,8 @@ function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 
-var colors = document.getElementById("rainbow");
-var array = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
+const colors = document.getElementById("rainbow");
+const  array = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
 
 for(var i = 0; i < array.length; i ++;){
   colors[i].style.color = array[i];
