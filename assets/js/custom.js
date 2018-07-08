b736c6e2f7dc36f93ef6f7b193854d75cdec8864
kajal <kajolsingh88088@gmail.com>
2018-07-08 17:58:11 +0530

    array in js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,11 +5,11 @@ function myFunction() {
 }
 
  var i = 0;
-function myFunction(){
+function mylisting(){
 const listItems = document.getElementById('rainbow');
 const colors = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
 
-for(i = 0; i < colors.length; i ++) {
+for(var i = 0; i < colors.length; i ++) {
   listItems[i].style.color = colors[i];
 }
 }
