29f7e943b6367c8d7e77c487878121380942ddaa
kajal <kajolsingh88088@gmail.com>
2018-07-08 17:37:58 +0530

    array in js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,9 +4,9 @@ function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 
-const colors = document.getElementById("rainbow");
-const  diffrent = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
+const listItems = document.getElementById('rainbow');
+const colors = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
 
-for(var i = 0; i < diffrent.length; i++;){
-  colors[i].style.color = diffrent[i];
+for(var i = 0; i < colors.length; i ++) {
+  listItems[i].style.color = colors[i];
 }
